4909e50cab95e7be56b410104b3b16f1960da7f3
coderarjun-1357 <99395755+coderarjun-1357@users.noreply.github.com>
2023-04-15 18:37:12 +0530

    Add files via upload

---
+++ Bottle.js
@@ -0,0 +1,43 @@
+Status = "";
+Bottle_image = "";
+objects = [];
+
+function preload(){
+    Bottle_image = loadImage("Bottle.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    object_Detector.detect(Bottle_image,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+}
+
+function draw(){
+    image(Bottle_image,0,0,640,350);
+    if(Status != ""){
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+
+            fill("#fc0303");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%",objects[i].x - 180, objects[i].y - 200);
+            noFill();
+            stroke("#fc0303");
+            rect(objects[i].x - 180, objects[i].y - 200, objects[i].width - 2693, objects[i].height - 1750);
+        }
+    }
+}
